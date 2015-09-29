01697f5d98b9fed764c15afe3608a2a4253644cb
Start Coding Student 2 <session2@START-Coding-MBP-C02PP742G8WN.local>
2015-09-28 18:02:31 -0700

    I changed the var totalDamage from 8 to 0

---
+++ DragonSlayer.js
@@ -0,0 +1,30 @@
+/**
+ * Created by session2 on 9/28/15.
+ */
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+var totalDamage = 0;
+while(slaying) {
+    if (youHit) {
+        console.log("You Hit!");
+        totalDamage += damageThisRound;
+        if(totalDamage >= 1) {
+            console.log("You win!");
+            slaying = false;
+        } else {
+            youHit = Math.floor(Math.random() * 2);
+        }
+    } else {
+        console.log("You lose!");
+        slaying = false;
+    }
+
+    console.log("You missed!");
+    slaying = false;
+}
+if (youHit == 1) {
+    console.log("You won!");
+} else {
+    console.log("You died!");
+}
